c711829c97377a9c0a626e25f049f54ed826254b
SalllesAndr <salllesandrr@gmail.com>
2024-12-07 12:42:34 -0300

    Commit Initial

---
+++ estruturas_control.py
@@ -0,0 +1,15 @@
+idade = 20
+if idade < 18:
+    print("Menor de idade.")
+
+elif idade >= 18 and idade < 60:
+    print("Adulto.")
+
+elif idade > 15:
+    print("Maior de 15 anos.")
+
+elif idade == 60:
+    print("Parabéns pelos 60 anos!")
+
+else:
+    print("Você é um idoso.")    
