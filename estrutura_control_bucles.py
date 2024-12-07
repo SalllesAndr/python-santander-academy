445e33b4775675f11594c7f794fb8f9d9b79ad30
SalllesAndr <salllesandrr@gmail.com>
2024-12-07 12:42:34 -0300

    Commit Initial

---
+++ estrutura_control_bucles.py
@@ -0,0 +1,9 @@
+print("Numeros de 1 a 10 multiplos de 2 com ciclo for:")
+for i in range(1, 11):
+    print(i * 2)
+
+print("\nNumeros de 1 a 10 multiplos de 2 com ciclo while:")
+contador = 1
+while contador <= 10:
+    print(contador * 2)
+    contador += 1
